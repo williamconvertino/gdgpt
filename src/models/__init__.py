c5bbf75cd142722f1d905db01acb9506055b5199
William Convertino <williamconvertino@gmail.com>
2024-12-11 11:40:19 -0500

    Added max model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,4 @@
 from src.models.gdgpt import GDGPT, GDGPTConfig
 from src.models.gdgpt_plus import GDGPTPlus, GDGPTPlusConfig
 from src.models.gdgpt_ultra import GDGPTUltra, GDGPTUltraConfig
+from src.models.gdgpt_max import GDGPTMax, GDGPTMaxConfig
