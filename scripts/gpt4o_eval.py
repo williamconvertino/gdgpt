34720268c371ec27fde190ccd93b374f15c4e416
William Convertino <williamconvertino@gmail.com>
2025-01-01 11:42:54 -0500

    Removed missing referene

--- scripts/gpt4o_eval.py
+++ scripts/gpt4o_eval.py
@@ -1,20 +1,20 @@
-import setup_paths
-import torch
-from util import get_model_from_args, get_tokenizer_and_dataset_from_args, load_most_recent_checkpoint
+# import setup_paths
+# import torch
+# from util import get_model_from_args, get_tokenizer_and_dataset_from_args, load_most_recent_checkpoint
 
-from src.evaluation import generate_gpt4o_inputs
-from src.datasets import TinyStoriesDataset
-from src.tokenizers import TinyStoriesTokenizer
+# from src.evaluation import generate_gpt4o_inputs
+# from src.datasets import TinyStoriesDataset
+# from src.tokenizers import TinyStoriesTokenizer
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
   
-  torch.manual_seed(0)
+#   torch.manual_seed(0)
   
-  model = get_model_from_args()
-  model, _ = load_most_recent_checkpoint(model)
+#   model = get_model_from_args()
+#   model, _ = load_most_recent_checkpoint(model)
   
-  # Load tokenizer and datasets
-  tokenizer, _, _, test_dataset = get_tokenizer_and_dataset_from_args(model.config.context_size)
+#   # Load tokenizer and datasets
+#   tokenizer, _, _, test_dataset = get_tokenizer_and_dataset_from_args(model.config.context_size)
   
-  # Train the model
-  generate_gpt4o_inputs(model, tokenizer, test_dataset, num_generations=10)
+#   # Train the model
+#   generate_gpt4o_inputs(model, tokenizer, test_dataset, num_generations=10)
