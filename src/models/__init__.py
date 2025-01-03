ae917cf1fb870fce67c9fec7903445bf169086e4
William Convertino <williamconvertino@gmail.com>
2025-01-03 10:45:53 -0500

    Added M3

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,3 +3,4 @@ from src.models.gd_tok import GDTok, GDTokConfig
 from src.models.gpt import GPT, GPTConfig
 from src.models.m1 import M1, M1Config
 from src.models.m2 import M2, M2Config
+from src.models.m3 import M3, M3Config
