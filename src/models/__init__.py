030e7c692c0b5c21fd3781f0ae7db8f1a1188bf9
William Convertino <williamconvertino@gmail.com>
2025-01-03 10:28:38 -0500

    Fixed imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
 from src.models.gd import GD, GDConfig
 from src.models.gd_tok import GDTok, GDTokConfig
 from src.models.gpt import GPT, GPTConfig
-from models.m1 import M1, M1Config
+from src.models.m1 import M1, M1Config
