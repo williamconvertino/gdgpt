bc24d8fce7b8437d6953a53f29efac165530fd6a
William Convertino <williamconvertino@gmail.com>
2025-01-03 10:35:08 -0500

    Added M2

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from src.models.gd import GD, GDConfig
 from src.models.gd_tok import GDTok, GDTokConfig
 from src.models.gpt import GPT, GPTConfig
 from src.models.m1 import M1, M1Config
+from src.models.m2 import M2, M2Config
