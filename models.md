7693123f3db9f1d175eab9967f5bff9875104e51
William Convertino <williamconvertino@gmail.com>
2025-01-13 14:04:34 -0500

    Added gpt4o eval

--- models.md
+++ models.md
@@ -1,11 +1,11 @@
 NO_LN, NO FF
 
 GPT - Pure GPT 2.12
-mid_1 - Using adjusted matrices 2.10
+mid_1 - Using adjusted matrices 2.10 // 2.18
 mid_2 - Subtracting E(W_c) 2.14
 mid_3 - Using diagonal matrices 2.26
 mid_4 - Using shared (NON diagonal) matrices 2.27
 mid_5 - Using shared DIAGONAL matrices 2.31
 mid_6 - Removing W_v 2.32
-mid_7 - using 1/N term 2.46
+mid_7 - using 1/N term 2.46 // 2.84
 mid_8 - PPE 3.83
--- src/evaluation/gpt4o_eval.py
+++ src/evaluation/gpt4o_eval.py
@@ -1,160 +1,148 @@
-# import torch
-# import os
-# import time
+import torch
+import os
+import time
 
-# from src.util import get_time_remaining
+from src.util import get_time_remaining
 
-# EVAL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/evaluations')
+INPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/evaluations/input')
+OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data/evaluations/output')
 
-# MODEL = 'gpt-4o'
+MODEL = 'gpt-4o-mini'
 
-# SYSTEM_PROMPT = "You are an expert writing evaluator designed to assess student story completions. Your role is to provide constructive, fair, and detailed evaluations based on specific rubric criteria."
+SYSTEM_PROMPT = "You are an expert writing evaluator designed to assess student story completions. Your role is to provide constructive, fair, and detailed evaluations based on specific rubric criteria."
 
-# USER_PROMPT = """
-# In the following exercise, the student is given a pre-written beginning of a story. The student needs to complete this story. The exercise tests the student´s language abilities and creativity.
+USER_PROMPT = """
+In the following exercise, the student is given a pre-written beginning of a story. The student needs to complete this story. The exercise tests the student´s language abilities and creativity.
 
-# Here is the pre-written beginning:
+Here is the pre-written beginning:
 
-# <PROVIDED BEGINNING>
-# [STORY_BEGIN]
-# </PROVIDED BEGINNING>
+<PROVIDED BEGINNING>
+[STORY_BEGIN]
+</PROVIDED BEGINNING>
 
-# And here is the students response:
+And here is the students response:
 
-# <STUDENT RESPONSE>
-# [STORY_END]
-# </STUDENT RESPONSE>
+<STUDENT RESPONSE>
+[STORY_END]
+</STUDENT RESPONSE>
 
-# First, provide a concise qualitative assessment about the student's writing. Then, give the writing a grade out of 10. These assessments should be done for each of the following rubric items:
+First, provide a concise qualitative assessment about the student's writing. Then, give the writing a grade out of 10. These assessments should be done for each of the following rubric items:
 
-# 1. Grammar:
-# * Is the writing grammatically correct?
-# * Evaluate syntax, punctuation, and sentence structure.
-# 2. Consistency:
-# * Is the student's writing consistent with the provided beginning of the story?
-# * How well does the student complete the final sentence of the prescribed beginning?
-# 3. Plot:
-# * Does the plot of the student's writing make sense (regardless of the provided beginning)?
-# 4. Creativity: 
-# * How creative is the student's writing?
+1. Grammar:
+* Is the writing grammatically correct?
+* Evaluate syntax, punctuation, and sentence structure.
+2. Consistency:
+* Is the student's writing consistent with the provided beginning of the story?
+* How well does the student complete the final sentence of the prescribed beginning?
+3. Plot:
+* Does the plot of the student's writing make sense (regardless of the provided beginning)?
+4. Creativity: 
+* How creative is the student's writing?
 
-# Format your response as follows:
+Format your response as follows:
 
-# <GRAMMAR>
-# [Qualitative assessment of grammar]
-# </GRAMMAR>
-# <GRAMMAR_GRADE>
-# [Grade out of 10]
-# </GRAMMAR_GRADE>
+<GRAMMAR>
+[Qualitative assessment of grammar]
+</GRAMMAR>
+<GRAMMAR_GRADE>
+[Grade out of 10]
+</GRAMMAR_GRADE>
 
-# <CONSISTENCY>
-# [Qualitative assessment of consistency]
-# </CONSISTENCY>
-# <CONSISTENCY_GRADE>
-# [Grade out of 10]
-# </CONSISTENCY_GRADE>
+<CONSISTENCY>
+[Qualitative assessment of consistency]
+</CONSISTENCY>
+<CONSISTENCY_GRADE>
+[Grade out of 10]
+</CONSISTENCY_GRADE>
 
-# <PLOT>
-# [Qualitative assessment of plot]
-# </PLOT>
-# <PLOT_GRADE>
-# [Grade out of 10]
-# </PLOT_GRADE>
+<PLOT>
+[Qualitative assessment of plot]
+</PLOT>
+<PLOT_GRADE>
+[Grade out of 10]
+</PLOT_GRADE>
 
-# <CREATIVITY>
-# [Qualitative assessment of creativity]
-# </CREATIVITY>
-# <CREATIVITY_GRADE>
-# [Grade out of 10]
-# </CREATIVITY_GRADE>
+<CREATIVITY>
+[Qualitative assessment of creativity]
+</CREATIVITY>
+<CREATIVITY_GRADE>
+[Grade out of 10]
+</CREATIVITY_GRADE>
 
-# Provide your assessment below:
-# """
+Provide your assessment below:
+"""
 
-# def generate_gpt4o_inputs(model, tokenizer, test_dataset, num_generations=10):
+def get_request_object(custom_id, content):
+  return {
+        "custom_id": f"{custom_id}",
+        "method": "POST",
+        "url": "/v1/chat/completions",
+        "body": {
+          "model": MODEL,
+          "messages": [
+            {
+              "role": "system", "content": SYSTEM_PROMPT
+            },
+            {
+              "role": "user", "content": content
+            }
+          ],
+          "max_tokens": 1000
+        }
+      }
+
+def generate_gpt4o_inputs(model, tokenizer, test_dataset, num_generations=10):
   
-#   i = 0
-#   num_skipped = 0
-#   start_time = time.time()
+  i = 0
+  num_skipped = 0
+  start_time = time.time()
   
-#   eval_items = []
+  eval_items = []
   
-#   for batch in enumerate(test_dataset):
+  for batch in enumerate(test_dataset):
     
-#     if i >= num_generations:
-#       break
+    if i >= num_generations:
+      break
     
-#     sequence = batch[0]
+    sequence = batch[0]
 
-#     input_size = model.config.context_size // 2
+    input_size = model.config.context_size // 2
 
-#     model_input = sequence[:input_size]
+    model_input = sequence[:input_size]
     
-#     if tokenizer.eos_token_id in model_input: # Exclude sequences with incomplete prompts, to avoid confusion in the GPT-4o evaluation
-#       num_skipped += 1
-#       continue
+    if tokenizer.eos_token_id in model_input: # Exclude sequences with incomplete prompts, to avoid confusion in the GPT-4o evaluation
+      num_skipped += 1
+      continue
     
-#     with torch.no_grad():
-#       model.eval()
+    with torch.no_grad():
+      model.eval()
       
-#       story_begin = tokenizer.decode(model_input.tolist())
-#       story_true_end = tokenizer.decode(sequence[input_size:].tolist())
+      story_begin = tokenizer.decode(model_input.tolist())
+      story_true_end = tokenizer.decode(sequence[input_size:].tolist())
       
-#       beam_search_sequence = model.beam_search(model_input.unsqueeze(0))
-#       beam_search_sequence = beam_search_sequence[input_size:].tolist()
-#       eos_token_id = beam_search_sequence.index(tokenizer.eos_token_id) if tokenizer.eos_token_id in beam_search_sequence else len(beam_search_sequence)
-#       beam_search_sequence = beam_search_sequence[:eos_token_id] # Remove EOS token if present
-#       if len(beam_search_sequence) < 2: # Exclude sequences with less than 2 tokens, to avoid confusion in the GPT-4o evaluation
-#         num_skipped += 1
-#         continue
+      beam_search_sequence = model.beam_search(model_input.unsqueeze(0))
+      beam_search_sequence = beam_search_sequence[input_size:].tolist()
+      eos_token_id = beam_search_sequence.index(tokenizer.eos_token_id) if tokenizer.eos_token_id in beam_search_sequence else len(beam_search_sequence)
+      beam_search_sequence = beam_search_sequence[:eos_token_id] # Remove EOS token if present
+      if len(beam_search_sequence) < 2: # Exclude sequences with less than 2 tokens, to avoid confusion in the GPT-4o evaluation
+        num_skipped += 1
+        continue
 
-#       story_beam_end = tokenizer.decode(beam_search_sequence)
+      story_beam_end = tokenizer.decode(beam_search_sequence)
       
-#       true_prompt = USER_PROMPT.replace('[STORY_BEGIN]', story_begin).replace('[STORY_END]', story_true_end)
-#       beam_prompt = USER_PROMPT.replace('[STORY_BEGIN]', story_begin).replace('[STORY_END]', story_beam_end)
+      true_prompt = USER_PROMPT.replace('[STORY_BEGIN]', story_begin).replace('[STORY_END]', story_true_end)
+      beam_prompt = USER_PROMPT.replace('[STORY_BEGIN]', story_begin).replace('[STORY_END]', story_beam_end)
       
-#       eval_items.append({
-#         "custom_id": f"request_{i}_true",
-#         "method": "POST",
-#         "url": "/v1/chat/completions",
-#         "body": {
-#           "model": MODEL,
-#           "messages": [
-#             {
-#               "role": "system", "content": SYSTEM_PROMPT
-#             },
-#             {
-#               "role": "user", "content": true_prompt
-#             }
-#           ],
-#           "max_tokens": 1000
-#         }
-#       })
+      eval_items.append(get_request_object(f"request_{i}_true", true_prompt))
+      eval_items.append(get_request_object(f"request_{i}_beam", beam_prompt))
       
-#       eval_items.append({
-#         "custom_id": f"request_{i}_beam",
-#         "method": "POST",
-#         "url": "/v1/chat/completions",
-#         "body": {
-#           "model": MODEL,
-#           "messages": [
-#             {
-#               "role": "system", "content": SYSTEM_PROMPT
-#             },
-#             {
-#               "role": "user", "content": beam_prompt
-#             }
-#           ],
-#           "max_tokens": 1000
-#         }
-#       })
+      i += 1
+      time_remaining = get_time_remaining(start_time, i, num_generations)
+      print(f"\r{i}/{num_generations} ({100 * i / num_generations:.2f}%) | Time Remaining: {time_remaining}", end='')
+    
+  os.makedirs(INPUT_DIR, exist_ok=True)
+  with open(f'{INPUT_DIR}/gpt4o_eval_input.jsonl', 'w') as f:
+    for item in eval_items:
+      f.write(f"{item}\n")
       
-#       i += 1
-#       time_remaining = get_time_remaining(start_time, i, num_generations)
-#       print(f"\r{i}/{num_generations} ({100 * i / num_generations:.2f}%) | Time Remaining: {time_remaining}", end='')
-      
-#   with open(f'{EVAL_DIR}/gpt4o_eval_input.jsonl', 'w') as f:
-#     for item in eval_items:
-#       f.write(f"{item}\n")
-      
-#   print(f"Generated {i} GPT-4o evaluation inputs, skipped {num_skipped} sequences.")
+  print(f"Generated inputs for GPT model:{MODEL}\n Processed {i}, skipped {num_skipped}.")
