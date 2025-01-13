72d75bc724d607d08ccb44eb3a7237f120badc65
William Convertino <williamconvertino@gmail.com>
2025-01-13 17:34:58 -0500

    Debugging

--- scripts/gpt4o_eval.py
+++ scripts/gpt4o_eval.py
@@ -2,7 +2,7 @@ import setup_paths
 import torch
 from util import get_model_from_args, get_tokenizer_and_dataset_from_args, load_most_recent_checkpoint, get_flags_from_args
 
-from src.evaluation import generate_gpt4o_inputs, create_batch
+from src.evaluation import generate_gpt4o_inputs, create_batch, check_batch, cancel_batch, parse_batch
 from src.datasets import TinyStoriesDataset
 from src.tokenizers import TinyStoriesTokenizer
 
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     print("Creating batch")
     create_batch()
   elif 'check' in flags:
-    pass
+    check_batch()
   elif 'parse' in flags:
     pass
   else:
--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,2 +1,2 @@
 from src.evaluation.generation import evaluate_model_generation, quick_eval
-from src.evaluation.gpt4o_eval import generate_gpt4o_inputs, create_batch
+from src.evaluation.gpt4o_eval import generate_gpt4o_inputs, create_batch, check_batch, cancel_batch, parse_batch
