398907d4f6680b163274bb1c542887f5f936f3ea
William Convertino <williamconvertino@gmail.com>
2025-01-13 14:05:09 -0500

    Added gpt4o eval

--- src/evaluation/__init__.py
+++ src/evaluation/__init__.py
@@ -1,2 +1,2 @@
 from src.evaluation.generation import evaluate_model_generation, quick_eval
-# from src.evaluation.gpt4o_eval import generate_gpt4o_inputs
+from src.evaluation.gpt4o_eval import generate_gpt4o_inputs
